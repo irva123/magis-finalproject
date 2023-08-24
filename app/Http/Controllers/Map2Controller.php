8368d85404c76361b5a21d457546bd121d895955
tumblerd <atafama.121@gmail.com>
2023-08-24 07:56:10 +0700

    merge irvaptr

---
+++ app/Http/Controllers/Map2Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CentrePoint;
+use App\Models\Space;
+use Illuminate\Http\Request;
+
+class Map2Controller extends Controller
+{
+    public function index()
+    {
+        /**
+         *  Pada method index kita mengambil single data dari tabel centrepoint
+         *  Selanjutnya kita mengambil seluruh data dari tabel space untuk menampilkan marker yang akan
+         *  kita gtampilkan pada view map.blade 
+         */
+        $centrePoint = CentrePoint::get()->first();
+        $spaces = Space::get();
+        return view('map2',[
+            'spaces' => $spaces,
+            'centrePoint' => $centrePoint
+        ]);
+        //return dd($spaces);
+    }
+
+    public function show($nama)
+    {
+        /**
+         * Hampir sama dengam method index diatas
+         * tapi disini kita hanya akan menampilkan single data saja untuk space
+         * yang kita pilih pada view map dan selanjutnya kita akan di arahkan 
+         * ke halaman detail untuk melihat informasi lebih lengkap dari space
+         * yang kita pilih
+         */
+        $centrePoint = CentrePoint::get()->first();
+        $spaces = Space::where('nama',$nama)->first();
+        return view('detail2',[
+            'centrePoint' => $centrePoint,
+            'spaces' => $spaces
+        ]);
+    }
+}
---
+++ resources/views/detail2.blade.php
@@ -0,0 +1,105 @@
+
+<!--
+ // WEBSITE: https://themefisher.com
+ // TWITTER: https://twitter.com/themefisher
+ // FACEBOOK: https://www.facebook.com/themefisher
+ // GITHUB: https://github.com/themefisher/
+-->
+
+<!-- 
+THEME: Medic | Medical HTML Template
+VERSION: 1.0.0
+AUTHOR: Themefisher
+HOMEPAGE: https://themefisher.com/products/medic-medical-template/
+DEMO: https://demo.themefisher.com/themefisher/medic/
+GITHUB: https://github.com/themefisher/Medic-Bootstrap-Medical-Template
+WEBSITE: https://themefisher.com
+TWITTER: https://twitter.com/themefisher
+FACEBOOK: https://www.facebook.com/themefisher
+-->
+
+
+
+@extends('layouts.main')
+
+@section('navbar')
+  <!-- Contact Start -->
+        <div class="contact100-form-title container pb-10">
+        <div class="row mt-5">
+          <div class="col-12 text-center ">
+            <div class="row text-center mt-5">
+              <div class="col-lg-12">
+                <h3>{{ $spaces->nama }}</h3>
+                <hr>
+            </div>
+            </div>
+          <!-- advertisement section -->
+          <div class="container py-4 justify-content-center mt-3 px-4">
+          <div class="card-body">
+          <img class="img-fluid" width="500" height="300" src="{{ url('storage/'.$spaces->foto) }}"
+                            alt="">
+                    </div>    
+                        <p>
+                        <p class="black-text2">{{ $spaces->deskripsi }}</p>
+                        </p>
+                      </br>
+                      </br>
+                      </br>
+        <div class="row mb-10">
+            <div class="col-md-6 col-xs-6 text-left ">
+                <div class="card">
+                    <div class="card-body">
+
+                        <p>
+                        <h5 class="black-text"><strong>Alamat :</strong></h5>
+                        <p class="black-text3">{{ $spaces->alamat }}</p>
+                        </p>
+
+                        <p>
+                        <h5 class="black-text"><strong>Jam Operasional :</strong></h5>
+                        <p class="black-text3">{{ $spaces->jam_buka}}</p>
+                        </p>
+
+                        <p>
+                        <h5 class="black-text"><strong>Harga Hari Kerja :</strong></h5>
+                        <p class="black-text3">{{ $spaces->harga_weekday}}</p>
+                        </p>
+
+                        <p>
+                        <h5 class="black-text"><strong>Harga Akhir Pekan :</strong></h5>
+                        <p class="black-text3">{{ $spaces->harga_weekend}}</p>
+                        </p>
+
+                        <p>
+                        <h5 class="black-text"><strong>Kontak :</strong></h5>
+                        <p class="black-text3">{{ $spaces->kontak}}</p>
+                        </p>
+
+                        <p>
+                        <h5 class="black-text"><strong>Sosial Media :</strong></h5>
+                        <p class="black-text3">{{ $spaces->sosmed}}</p>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-xs-6">
+            <div class="card ">
+                    <div class="card-body">
+                    <iframe src="{{ $spaces->link_maps }}" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                  </div>
+            </div>
+        </div>
+        </div>
+    </div>
+	<!-- end advertisement section -->
+        </div>
+      </div>
+      </div>
+
+<!--====  End of Contact Form  ====-->
+
+@endsection
+
+</html>
